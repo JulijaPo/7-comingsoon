b02581cc8458072870d9089dd28f99bd71f2a282
Your Name <you@example.com>
2021-02-08 10:54:42 +0200

    Progress bar animation #14

--- js/components/ProgressBar.js
+++ js/components/ProgressBar.js
@@ -1,50 +1,101 @@
-class ProgressBar{
-    constructor(selector, data){
+class ProgressBar {
+    constructor(selector, data) {
         this.selector = selector;
-        this.data = data
-        this.DOM = null
-        this.init()  
-    }
-    init(){
-        if(!this.isValidSecektor()){
-            return false
-        }
-        this.render()
-    }
-    isValidSecektor(){
-    if(typeof this.selector !== 'string' || this.selector === ""){
-        console.error('ERROR: netinkamo formato selektorius')
-        return false
-    }
-    const DOM = document.querySelector(this.selector)
-    if(!DOM){
-        console.error('Error: nurodyto elemento pagal duota selektoriu nera')
-        return false
-    }
-this.DOM = DOM
-return true
+        this.data = data;
+
+        this.DOM = null;
+
+        this.init();
     }
 
-    generateProgressBar(progressBar){
+    init() {
+        if (!this.isValidSelector() ||
+            !this.isValidData()) {
+            return false;
+        }
+
+        this.render();
+    }
+
+    isValidSelector() {
+        if (typeof this.selector !== 'string' ||
+            this.selector === '') {
+            console.error('ERROR: netinkamo formato this.selector');
+            return false;
+        }
+
+        const DOM = document.querySelector(this.selector);
+        if (!DOM) {
+            console.error('ERROR:nurodyto elemento pagal duota this.selector nera.');
+            return false;
+        }
+        this.DOM = DOM;
+
+        return true;
+    }
+
+    isValidData() {
+        if (!Array.isArray(this.data) ||
+            this.data.length === 0) {
+            console.error('ERROR: netinkamo formato this.data');
+            return false;
+        }
+
+        return true;
+    }
+
+    isValidProgressBar(progressBar) {
+        if (typeof progressBar !== 'object' ||
+            Array.isArray(progressBar) ||
+            progressBar === null ||
+            !progressBar.label ||
+            typeof progressBar.label !== 'string' ||
+            progressBar.label.trim() === '' ||
+            typeof progressBar.value !== 'number' ||
+            !isFinite(progressBar.value) ||
+            progressBar.value > 100 ||
+            progressBar.value < 0) {
+            console.warn('WARNING: netinkamo formato objektas', progressBar);
+            return false;
+        }
+        return true;
+    }
+
+    generateProgressBar(progressBar) {
         return `<div class="progress-bar">
-                    <div class = "texts">
+                    <div class="texts">
                         <div class="label">${progressBar.label}</div>
-                        <div class="value"  >${progressBar.value}%</div>
+                        <div class="value">${this.formatNumber(progressBar.value)}%</div>
                     </div>
-                    <div class = "bar">
-                        <div class = "progress" style="width: ${progressBar.value}%">
-                            <div class = "loading"></div>
+                    <div class="bar">
+                        <div class="progress" style="width: ${progressBar.value}%;">
+                            <div class="loading"></div>
                         </div>
                     </div>
-                </div>`
+                </div>`;
+    }
+
+    formatNumber(number) {
+        return Math.round(number);
     }
 
     render() {
-    let HTML = ""
-    for ( const progress of this.data){
-        HTML += this.generateProgressBar(progress)
-    }
-    this.DOM.innerHTML += HTML
+        let HTML = '';
+
+        for (const progress of this.data) {
+            if (!this.isValidProgressBar(progress)) {
+                continue;
+            }
+            HTML += this.generateProgressBar(progress);
+        }
+
+        if (HTML === '') {
+            console.warn('WARNING: this.data neturi nei vieno validaus objekto');
+            return false;
+        }
+
+        this.DOM.innerHTML += HTML;
     }
 }
+
 export { ProgressBar }
--- js/data/progressBarData.js
+++ js/data/progressBarData.js
@@ -1,15 +1,80 @@
 const progressBarData = [
+/*141526,
+ 'asfd',
+  true,
+   false,
+   null,
+  undefined,
+   [],
+   {},
+   {
+       label: null
+   },
     {
-        label: 'UX disign',
+        label: ''
+    },
+    {
+        label: 1452
+    },
+    {
+        label: 'Programming'
+    },
+    {
+        label: 'Programming',
+        value: null
+    },
+    {
+        label: 'Programming',
+        value: 'random'
+    },
+    {
+        label: 'Programming',
+        value: Infinity
+    },
+    {
+        label: 'Programming',
+        value: NaN
+    },
+    {
+        label: 'Programming',
+        value: 120
+    },
+    {
+        label: 'Programming',
+        value: -120
+    },
+    {
+        label: 'Programming',
+        value: 50.148525145251
+    },
+    {
+        label: ' ',
+        value: 30
+    },
+    {
+        label: 'Programming',
+        value: 100
+    },
+    {
+        label: 'Programming',
+        value: 0
+    },
+    {
+        label: 'Programming',
+        value: -0
+    },*/
+    {
+        label: 'UX Design',
         value: 90
     },
     {
-        label: 'Web disign',
+        label: 'Web design',
         value: 85
     },
     {
         label: 'Web development',
         value: 86
-    }
+    },
 ];
-export { progressBarData}
+
+export { progressBarData }
