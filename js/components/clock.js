67721b482523e9c40f7acd2176dd01dcbf71c7d0
Your Name <you@example.com>
2021-02-09 10:56:16 +0200

    Update clock #34

--- js/components/clock.js
+++ js/components/clock.js
@@ -1,7 +1,25 @@
-function clock(selector){ 
+function clock(selector, deadline){ 
+    //input validation
+    if (typeof selector !== "string" ||
+    selector === ''){
+    console.error('Error: netinkamo formato selektoris')
+    return false
+    }
+    if (typeof deadline !== "string" ||
+    deadline === "" ||
+    !isFinite((new Date(`2000-${deadline}`)).getTime())){
+    console.error('Error: netinkamo formato data')
+    return false
+    }
+    //logic
+
     const DOM = document.querySelector(selector)
+    if(!DOM){
+        console.error('Error: pagal pateikta selektoriu nepavyko rasti norimo Dom')
+        return false
+    }
     let allValueDOM = null
-    const deadline = '01-04 14:00:00'
+
     let numbers = calcTime(deadline)
     const labels = ['days', 'hours', 'minutes', 'secodns']
     let HTML = ''
@@ -11,25 +29,21 @@ function clock(selector){
         <div class = 'label'>${labels[i]}</div>
         </div>`
     }
-
     DOM.innerHTML = HTML
     allValueDOM = document.querySelectorAll(`${selector} .value`)
-setInterval(function(){
+    setInterval(function(){
     numbers = calcTime(deadline)
     for(let i = 0; i<4;i++){
-        allValueDOM[i].innerText =  numberFormat(numbers[i])
+        allValueDOM[i].innerText = numberFormat(numbers[i])
     }
-
 },1000)
 }
 function numberFormat(numbers){
     if(numbers < 10){
-
     return '0'+ numbers
 }
  return numbers
 }
-
 function calcTime(deadline){
     const date = new Date()
     const now = Date.now()
@@ -50,7 +64,5 @@ if(fullDeadlineInMiliseconds < now){
   const minutes = Math.floor(unusedSeconds / 60 )
   unusedSeconds -= minutes * 60 
 return [days, hours, minutes, unusedSeconds]
-
 }
-
 export { clock }
